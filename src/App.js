de2b6ab6dfac1374dd5fb2779186f9fce98a9d23
asifali-7890 <78083087+asifali-7890@users.noreply.github.com>
2024-09-23 09:46:27 +0530

    New commited wow

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import appStore from './utils/appStore.js';
 
 function App() {
   return <Provider store={appStore}>
-    <Body /> {/* //This is a Body  */}
+    <Body /> {/* //This is a Body body */}
   </Provider>;
 }
 export default App;
